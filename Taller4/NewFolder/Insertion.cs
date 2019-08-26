479ef2c6bc05f7248fcd0a114c4f65cc154d338c
Juan Sebastian Puerta Ordonez <1007672072@academico.icesi.edu.co>
2019-08-26 15:10:05 -0500

    I added the Insertion method

--- Taller4/NewFolder/Insertion.cs
+++ Taller4/NewFolder/Insertion.cs
@@ -6,5 +6,20 @@ namespace Taller4.NewFolder
 {
     class Insertion
     {
+        public void insertionSort(int[] arreglo) {
+            int n = arreglo.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arreglo[i];
+                int j = i - 1;
+
+                while (j >= 0 && arreglo[j] > key)
+                {
+                    arreglo[j + 1] = arreglo[j];
+                    j = j - 1;
+                }
+                arreglo[j + 1] = key;
+            }
+        }
     }
 }
